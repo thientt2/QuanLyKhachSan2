0dbe986937198169861298ecc5e4c5eacd34608e
thientt2 <tt219007@gmail.com>
2023-11-25 23:24:04 +0700

    Hello

---
+++ QLKhachSan/ViewModel/ControlBarViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QLKhachSan.ViewModel
+{
+    public class ControlBarViewModel:BasicViewModel
+    {
+        #region commands
+        public ICommand CloseWindowCommand { get; set; }
+        #endregion
+        public ControlBarViewModel()
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) => { GetWindowParent(p); });
+        }
+        void GetWindowParent(UserControl p)
+        {
+            FrameworkElement t = p.Parent as FrameworkElement;
+            
+        }
+    }
+}
---
+++ QLKhachSan/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QLKhachSan.ViewModel
+{
+    public class MainViewModel:BasicViewModel
+    {
+        public bool IsLoaded = false;
+        public MainViewModel()
+        {
+            
+            if (!IsLoaded)
+            {
+                IsLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+        }
+        
+    }
+}
