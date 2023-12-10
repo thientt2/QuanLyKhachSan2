08cd70a948d5efe2ac6e3738e2302b01d45f3d63
bone204 <truongbmt4@gmail.com>
2023-12-10 14:42:15 +0700

    HT 10/12 Dreamy

--- QLKhachSan/MainWindow.xaml.cs
+++ QLKhachSan/MainWindow.xaml.cs
@@ -20,11 +20,44 @@ namespace QLKhachSan
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {        
+    {   
         public MainWindow()
         {
             InitializeComponent();
-            datePicker.SelectedDate = DateTime.Now;
+        }
+
+        private void CancelButton_Client(object sender, RoutedEventArgs e)
+        {
+            txtMAKH.Text = "";
+            txtTENKH.Text = "";
+            txtGIOITINHKH.Text = "";
+            txtSDTKH.Text = "";
+            txtEMAILKH.Text = "";
+            txtQUOCTICH.Text = "";
+            txtSOCCCD.Text = "";
+            txtDIACHIKH.Text = "";
+            txtNGSINHKH.Text = "";
+        }
+
+        private void CancelButton_Staff(object sender, RoutedEventArgs e)
+        {
+            txtMANV.Text = "";
+            txtTENNV.Text = "";
+            txtGIOITINHNV.Text = "";
+            txtSDTNV.Text = "";
+            txtEMAILNV.Text = "";
+            txtDIACHINV.Text = "";
+            txtVITRILAMVIECNV.Text = "";
+            txtLUONGNV.Text = "";
+            txtNGVLNV.Text = "";
+            txtNGSINHNV.Text = "";
+        }
+
+        private void CancelButton_Service(object sender, RoutedEventArgs e)
+        {
+            txtMADV.Text = "";
+            txtTENDV.Text = "";
+            txtDONGIA.Text = "";
         }
     }
 }
