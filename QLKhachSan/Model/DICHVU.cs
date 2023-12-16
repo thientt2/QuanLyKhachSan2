f41b7b040812ed241711693cea6f8bf69e7f0b36
thientt2 <tt219007@gmail.com>
2023-12-16 13:40:41 +0700

    TT

--- QLKhachSan/Model/DICHVU.cs
+++ QLKhachSan/Model/DICHVU.cs
@@ -9,25 +9,21 @@
 
 namespace QLKhachSan.Model
 {
-    using QLKhachSan.ViewModel;
     using System;
     using System.Collections.Generic;
     
-    public partial class DICHVU : BasicViewModel
+    public partial class DICHVU
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public DICHVU()
         {
             this.CTPDVs = new HashSet<CTPDV>();
         }
-
-        private string _MADV;
-        public string MADV { get { return _MADV; } set { _MADV = value; OnPropertyChanged(); } }
-        private string _TENDV;
-        public string TENDV { get { return _TENDV; } set { _TENDV = value; OnPropertyChanged(); } }
-        private decimal? _DONGIA;
-        public decimal? DONGIA { get { return _DONGIA; } set { _DONGIA = value; OnPropertyChanged(); } }
-
+    
+        public string MADV { get; set; }
+        public string TENDV { get; set; }
+        public Nullable<decimal> DONGIA { get; set; }
+    
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<CTPDV> CTPDVs { get; set; }
     }
--- QLKhachSan/Model/PHIEUDATPHONG.cs
+++ QLKhachSan/Model/PHIEUDATPHONG.cs
@@ -9,12 +9,10 @@
 
 namespace QLKhachSan.Model
 {
-    using QLKhachSan.ViewModel;
     using System;
     using System.Collections.Generic;
-    using System.Data.Entity.Core.Common.CommandTrees;
-
-    public partial class PHIEUDATPHONG : BasicViewModel
+    
+    public partial class PHIEUDATPHONG
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public PHIEUDATPHONG()
@@ -23,19 +21,13 @@ namespace QLKhachSan.Model
             this.HOADONs = new HashSet<HOADON>();
             this.PHIEUDICHVUs = new HashSet<PHIEUDICHVU>();
         }
-
-        private string _MAPDP;
-        public string MAPDP { get { return _MAPDP; } set { _MAPDP = value; OnPropertyChanged(); } }
-        private string _MAKH;
-        public string MAKH { get { return _MAKH; } set { _MAKH = value; OnPropertyChanged(); } }
-        private string _MANV;
-        public string MANV { get { return _MANV; } set { _MANV = value; OnPropertyChanged(); } }
-
-        private DateTime? _NGDAT;
-        public DateTime? NGDAT { get { return _NGDAT; } set { _NGDAT = value; OnPropertyChanged(); } }
-        private DateTime? _NGNHAN;
-        public DateTime? NGNHAN { get { return _NGNHAN; } set { _NGNHAN = value; OnPropertyChanged(); } }
-
+    
+        public string MAPDP { get; set; }
+        public string MAKH { get; set; }
+        public string MANV { get; set; }
+        public Nullable<System.DateTime> NGDAT { get; set; }
+        public Nullable<System.DateTime> NGNHAN { get; set; }
+    
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<CTPDP> CTPDPs { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
